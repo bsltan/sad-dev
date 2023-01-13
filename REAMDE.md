0870f7e3f00a5cf98c6cb3d5e9f3d6661ba4ab56
bsltan <califchichi@yahoo.com>
2023-01-13 20:18:06 +0300

    Add files via upload

---
+++ REAMDE.md
@@ -0,0 +1,2 @@
+# sad-dev
+## add console.log
---
+++ test.js
@@ -0,0 +1,12 @@
+function hello(){
+    console.log("hello world");
+}
+hello();
+
+if(true){
+    console.log("true");
+}else{
+    console.log("false");
+}
+
+hello();
